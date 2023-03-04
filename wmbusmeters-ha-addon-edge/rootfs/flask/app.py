0b6c2f0f02e311e203e4bcf51f6022ca61602b33
BIBOLV <49994376+BIBOLV@users.noreply.github.com>
2023-03-04 02:10:32 +0200

    code

--- wmbusmeters-ha-addon-edge/rootfs/flask/app.py
+++ wmbusmeters-ha-addon-edge/rootfs/flask/app.py
@@ -43,4 +43,4 @@ def get_json():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="127.0.0.1", port=5000)
